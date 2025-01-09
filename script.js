d62a0579233ed159314898c6bb768a6740dd639a
JM-Bass <joaomariapereire@gmail.com>
2025-01-09 10:14:37 -0300

    Primeiro commit do Projeto Discover

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  // if(html.classList.contains('light')) {
+  //  html.classList.remove('light')
+  //} else {
+  //  html.classList.add('light')
+  // }
+
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Foto light")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo."
+    )
+  }
+}
